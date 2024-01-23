27b4ffbe79f5381031a440eb9292da3b3740120e
yingn02 <kiwi131024@gmail.com>
2024-01-23 14:06:02 +0900

    메인화면 맵 생성

--- capstone/Assets/mainmenu/csh_mainmenu.cs
+++ capstone/Assets/mainmenu/csh_mainmenu.cs
@@ -6,21 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class csh_click : MonoBehaviour
 {
+    public void start_btn_clicked() {
+        SceneManager.LoadScene("SampleScene");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetMouseButton(0)) {
-        //    SceneManager.LoadScene("SimpleScene");
-        //}
 
-        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame) {
-            SceneManager.LoadScene("SampleScene");
-        }
     }
 }
