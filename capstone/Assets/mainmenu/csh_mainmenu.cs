cd9c4a9bd2a3418a897ff7ab43327fbf3f548e7e
yingn02 <kiwi131024@gmail.com>
2024-01-24 02:32:40 +0900

    메인화면 완료

--- capstone/Assets/mainmenu/csh_mainmenu.cs
+++ capstone/Assets/mainmenu/csh_mainmenu.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
-public class csh_click : MonoBehaviour
+public class csh_mainmenu : MonoBehaviour
 {
+    public GameObject start_btn;
+
     public void start_btn_clicked() {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("SelectMenu");
     }
 
     // Start is called before the first frame update
