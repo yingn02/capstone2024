7a75f5db23bc4d9a43f0b89b62a297be39337d51
Thenaidn <kjss0913@naver.com>
2024-01-25 15:16:41 +0900

    밝기 조절
    스크롤바로 밝기 조절

--- capstone/Assets/Scripts/optionPanel.cs
+++ capstone/Assets/Scripts/optionPanel.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class optionPanel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
     public void optionToggle()
     {
         if(this.gameObject.activeSelf) this.gameObject.SetActive(false);
