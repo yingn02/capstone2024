3e7b2ad30f09a39de8d53476f492d1e0570e04e6
hs-1971261-kimjeongseok <53766785+hs-1971261-kimjeongseok@users.noreply.github.com>
2024-04-03 14:10:59 +0900

    활이 컨트롤러 위치로 이동
    왼쪽 컨트롤러의 grab 버튼을 누르면 활이 컨트롤러 위치로 이동

---
+++ capstone/Assets/1Scene/Scripts/bowControl.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
+
+public class bowControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject bow;
+    public InputActionReference grabAction;
+    public GameObject leftController;
+
+    private bool isGrabbing = false;
+
+    void OnEnable()
+    {
+        grabAction.action.performed += GrabPerformed;
+        grabAction.action.canceled += GrabCanceled;
+        grabAction.action.Enable();
+    }
+
+    void OnDisable()
+    {
+        grabAction.action.performed -= GrabPerformed;
+        grabAction.action.Disable();
+    }
+
+    void GrabPerformed(InputAction.CallbackContext context)
+    {
+        Debug.Log("grabstart");
+        isGrabbing = true;
+        RemoveControllerModel();
+    }
+    void GrabCanceled(InputAction.CallbackContext context)
+    {
+        Debug.Log("grabend");
+        isGrabbing = false;
+        RestoreControllerModel();
+    }
+    void MoveBowToControllerPosition()
+    {
+        bow.transform.position = leftController.transform.position;
+        bow.transform.rotation = leftController.transform.rotation;
+    }
+
+    void RemoveControllerModel()
+    {
+        Renderer[] renderers = leftController.GetComponentsInChildren<Renderer>();
+        foreach (Renderer renderer in renderers)
+        {
+            renderer.enabled = false;
+        }
+    }
+    void RestoreControllerModel()
+    {
+        Renderer[] renderers = leftController.GetComponentsInChildren<Renderer>();
+        foreach (Renderer renderer in renderers)
+        {
+            renderer.enabled = true;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGrabbing)
+        {
+            MoveBowToControllerPosition();
+        }
+    }
+}
